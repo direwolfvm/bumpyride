f15c21d92cf55bd3eda713340b014a5a89213f74
jeccles-pif <jordan.eccles@gsa.gov>
2026-04-21 14:47:34 -0400

    Initial Commit

---
+++ BumpyRide/BumpyRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumpyRideApp.swift
+//  BumpyRide
+//
+//  Created by Jordan Eccles on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct BumpyRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
